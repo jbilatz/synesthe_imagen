d7b9687bcf26ef6eb95c5e03e854f45fb60d791d
jbilatz <j@javierbilatz.com.ar>
2020-06-28 15:07:54 -0300

    anda pero lento y bardea el reconcimiento de la nota

--- synesthe_1b_p_19_6.js
+++ synesthe_1b_p_19_6.js
@@ -1,45 +1,18 @@
 const model_url = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/';
 
-let pitch;  // el detector
-let fd;     // ;p detectadp
-let mic;    // el mic
-
-
-let notes = [
-{
-note: 
-  'A', 
-  freq: 
-  440
-}
-, 
-{
-note: 
-  'E', 
-  freq: 
-  329.6276
-}
-, 
-{
-note: 
-  'C', 
-  freq: 
-  261.6256
-}
-, 
-{
-note: 
-  'G', 
-  freq: 
-  391.9954
-}
-];
-
-
 var figuras = [];
 
-let r;
-let img;
+let pitch;  // el detector
+let fd;
+let mlevel, micNivelRegistrado;     // ;p detectadp
+let mic;    // el mic
+let midiNote;
+let indiceFigura;
+let transparencia;
+let mostrarFiguras;
+let unbralSupMic, umbralInfMic;
+
+// let r;
 let posXfigs, posYfigs, anchoFigs, altoFigs, factorFigs;
 var canRender = false;
 
@@ -49,21 +22,21 @@ function preload() {
   }
 }
 
-
 function setup() {
   console.log('setup. creating canvas:');
   
   cnv = createCanvas(windowWidth, windowHeight);
   cnv.mousePressed(initAudio);  
   // muestra una indicacion de lo que tiene que hacer el usuario
-  text('tap here and enable mic to begin', 10, 20, width - 20);
-
+  textSize(16);
+  textLeading(30); // Set leading to 10
+  text('clickear/tocar aquí para habilitar el mic y empezar\nclick/tap here and enable mic to begin', 40, 30, width - 40);
   
   imageMode(CENTER);
   posXfigs = width/2;
   posYfigs = height/2;
 
-  r = int(random(28));
+  // r = int(random(28));
 
   factorFigs = 1.2432;
 
@@ -75,6 +48,13 @@ function setup() {
     altoFigs = width/factorFigs;
   }
   console.log('nos vimos...');
+
+  transparencia = 0;
+  mostrarFiguras = false;
+
+  unbralSupMic = 0.015;
+  umbralInfMic = 0.003;
+  indiceFigura = 0;
 }
 
 
@@ -91,7 +71,7 @@ function initAudio(){
 function listening() {
   console.log('listening');
   pitch = ml5.pitchDetection(model_url, audioContext, mic.stream, modelLoaded);
-  console.log(pitch);
+  // console.log(pitch);
 }
 
 function modelLoaded() {
@@ -104,10 +84,10 @@ function modelLoaded() {
 function getPitch() {
   pitch.getPitch(function(err, frequency) {
     if (frequency) {
-      console.log(frequency);
+      // console.log("frequency: "+frequency);
       fd = frequency;
     } else {
-      console.log('no pitch');
+      // console.log('no pitch');
     }
     getPitch();
   })
@@ -118,26 +98,68 @@ function draw() {
   if( !canRender ) return;
   
   background(0);
+  
+  // console.log(typeof pitch);
+  mlevel = mic.getLevel();
+  
+  // console.log('mic level: '+mic.getLevel());
+  // console.log('mic level es un número?: ' + !isNaN(mlevel) );
+  // var t = map(fd, 800, 1600, 0, 255);
 
+  if(mlevel > unbralSupMic){
+    console.log('mic level: '+mic.getLevel());
+    micNivelRegistrado = mlevel;
+
+    mostrarFiguras = true;
+    console.log('mostrarFiguras: '+mostrarFiguras);
+
+    midiNote = int(69+12*(log((fd)/440)));
+    console.log('midiNote: '+midiNote);
+    indiceFigura = midiNote - 48;
+    console.log('indiceFigura: '+indiceFigura);
+    fill(255);
+    text('nota: ' + midiNote, 40, 30, width - 40);
   
 
-  if (frameCount%30 == 0) {
-    r = int(random(28));
+  }
+
+  // var transp = 255;
+
+  if(mostrarFiguras){
+
+    if(!isNaN(mlevel)){
+
+      transparencia = map(mlevel, 0, micNivelRegistrado, 0, 255);
+      console.log('transparencia: '+transparencia);
+    }
+
+
+
+    push();
+
+    tint(255, transparencia);
+    if(indiceFigura =>0 && indiceFigura < figuras.length ){
+
+      image(figuras[indiceFigura], posXfigs, posYfigs, anchoFigs, altoFigs);
+
+    }
+  
+    pop();
+
+    if(mlevel < umbralInfMic){
+      mostrarFiguras = false;
+      console.log('mostrarFiguras: '+mostrarFiguras);
+    }
+
   }
 
   push();
-  
-  console.log(typeof pitch);
-
-  var t = map(fd, 800, 1600, 0, 255);
-  tint(255, t);
-  
-  
-  image(figuras[r], posXfigs, posYfigs, anchoFigs, altoFigs);
 
   tint(255);
   image(figuras[28], posXfigs, posYfigs, anchoFigs, altoFigs);
 
   pop();
+
+
   
 }
